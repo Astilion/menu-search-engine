746f6bdc6445dc3fca230c88c49d039ffbaf91b3
Mateusz <mkaminski.contact@gmail.com>
2023-06-27 17:52:10 +0200

    scripted app in js

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,14 @@
-console.log("hello world5");
+const searchInput = document.querySelector('.search');
+const drinkList = document.querySelectorAll('li');
+const searchHandler = () => {
+    const text = searchInput.value.toLowerCase();
+    drinkList.forEach(el => {
+        if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+            el.style.display = 'block';
+        }
+        else {
+            el.style.display = 'none';
+        }
+    });
+};
+searchInput.addEventListener('keyup', searchHandler);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("hello world5");
+const searchInput: HTMLInputElement = document.querySelector('.search');
+const drinkList = document.querySelectorAll('li');
+
+
+const searchHandler = () => {
+    const text:string = searchInput.value.toLowerCase();
+    drinkList.forEach(el => {
+        if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+            el.style.display = 'block'
+        } else {
+            el.style.display = 'none'
+        }
+
+    })
+}
+
+searchInput.addEventListener('keyup', searchHandler)
